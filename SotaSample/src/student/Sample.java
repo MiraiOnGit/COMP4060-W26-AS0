459568da1613f4418e90f54d0d46492b9e04eefa
MiraiOnGit <Minh.Nguyen1@umanitoba.ca>
2026-01-15 21:47:05 -0600

    Add section about SOTA

--- SotaSample/src/student/Sample.java
+++ SotaSample/src/student/Sample.java
@@ -1,5 +1,6 @@
 package student;
 import java.awt.Color;
+import java.io.File;
 
 import jp.vstone.RobotLib.*;
 
@@ -36,29 +37,74 @@ public class Sample {
 
 			// example of how to set poses in bulk. Store the pose information into the pose object.
 			pose = new CRobotPose();
-			pose.SetPose(new Byte[] {1   ,2   ,3   ,4   ,5   ,6   ,7   ,8}	//id
-			,  new Short[]{80   , 80 , 80   , 80 ,80   ,80   ,80   ,80}				//target pos
-					);
+//			pose.SetPose(new Byte[] {
+//							1, //body yaw
+//							2, //left shoulder
+//							3, //left elbow
+//							4, //right shoulder
+//							5, //right elbow
+//							6, //head yaw
+//							7, //head pitch
+//							8 //head roll
+//			}	//id
+//			,  new Short[]{0   , 0 , 0   , 0 ,0   ,0   ,0   ,0}				//target pos
+//			);
 
 			// illuminate LEDS:  left eye, right eye, mouth, power button
 			// all are RGB (use Java.colors) except mouth which is 8 bit brightness
-			pose.setLED_Sota(Color.RED, Color.RED, 255, Color.RED);
+			pose.setLED_Sota(Color.MAGENTA, Color.MAGENTA, 255, Color.RED);
 
 			// apply the pose, do motion over 1000msec, linear interpolation
+//			motion.play(pose,1000);
+
+			// wait until motion has finished
+//			motion.waitEndinterpAll();   // also async public boolean isEndInterpAll()
+//			CRobotUtil.wait(500);   //pause the program / current thread
+
+			pose.SetPose(new Byte[] {
+							1, //body yaw
+							2, //left shoulder
+							3, //left elbow
+							4, //right shoulder
+							5, //right elbow
+							6, //head yaw
+							7, //head pitch
+							8 //head roll
+					}	//id
+					,  new Short[]{0   , 0 , 0   , 0 ,700   ,-500   ,400   ,0}				//target pos
+			);
+
 			motion.play(pose,1000);
 
 			// wait until motion has finished
 			motion.waitEndinterpAll();   // also async public boolean isEndInterpAll()
 			CRobotUtil.wait(500);   //pause the program / current thread
 
-			motion.ServoOff();
-			pose = new CRobotPose();
-			pose.setLED_Sota(Color.GREEN, Color.GREEN, 255, Color.GREEN);
+//			motion.ServoOff();
+//			pose = new CRobotPose();
+			pose.setLED_Sota(Color.MAGENTA, Color.MAGENTA, 255, Color.GREEN);
 			motion.play(pose,500);
 			motion.waitEndinterpAll();
-
-			CPlayWave.PlayWave(SOUNDS+"end_test.wav");
+			CPlayWave.PlayWave(SOUNDS+"ina-tomorrow.wav");
 			CRobotUtil.wait(2000);
+
+			pose.SetPose(new Byte[] {
+							1, //body yaw
+							2, //left shoulder
+							3, //left elbow
+							4, //right shoulder
+							5, //right elbow
+							6, //head yaw
+							7, //head pitch
+							8 //head roll
+					}	//id
+					,  new Short[]{0   , 0 , 0   , 0 ,0   ,0   ,0   ,0}				//target pos
+			);
+			// wait until motion has finished
+			motion.waitEndinterpAll();   // also async public boolean isEndInterpAll()
+			CRobotUtil.wait(500);   //pause the program / current thread
+
+			motion.ServoOff();
 		}
 	}
 }
