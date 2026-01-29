b0bfffb8fb1b1e9d4ce1249ab61ceb672cc27c79
youngmb <young@cs.umanitoba.ca>
2026-01-29 09:55:03 -0600

    piper library v1

--- SotaSample/src/sample4060/pipersample.java
+++ SotaSample/src/sample4060/pipersample.java
@@ -24,43 +24,29 @@ public class pipersample {
 
     public static void main(String[] args) throws IOException {
 
-        String urlString = "http://hri.cs.umanitoba.ca:5000";
-        String textToGen = "its a trap";
         String filename = "speech.wav";
 
-        System.out.println("Constructing the json RPC request");
-        URL url = new URL(urlString);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setDoOutput(true);
+        // more voices available, check https://huggingface.co/rhasspy/piper-voices/tree/main
+        // we can install if you want
+        listPiperVoices("http://hri.cs.umanitoba.ca:5000");
 
-        // JSON
-        String jsonInputString = "{\"text\": \""+textToGen+"\"," +   // see https://github.com/OHF-Voice/piper1-gpl/blob/main/docs/API_HTTP.md
-                                  "\"sample_rate\": 22050," +  // ensure match to Sota's working rate
-                "\"length_scale\": 1.0," +  // longer is slower
-                "\"noise_scale\": 0.7," +  // natural variation
-                "\"noise_w_scale\": 0.8}";  // phenome variation.
-        System.out.println("Connecting...");
-        try (OutputStream os = conn.getOutputStream()) {
-            byte[] input = jsonInputString.getBytes("utf-8");
-            System.out.println("Sending Request...");
-            os.write(input, 0, input.length);
-        }
 
-        System.out.println("Reading response...");
-        ByteArrayOutputStream wavBuffer = new ByteArrayOutputStream();
-        try (InputStream in = conn.getInputStream()) {
-            byte[] buffer = new byte[4096];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1) {
-                wavBuffer.write(buffer, 0, bytesRead);
-            }
-        }
-        System.out.println("Response received successfully");
+        PiperRequest piperRequest = new PiperRequest(
+                "http://hri.cs.umanitoba.ca:5000",
+                "its a trap",
+                null,  // or "en_GB-jenny_dioco-medium", or...
+                1,
+                0.667,
+                0.8
+        );
+        ByteArrayOutputStream wavData = getPiperWavData(piperRequest);
+
+        // once you get the wav data you can save it to a file for use / re-use, or just play it live
+        // I recommend writing a simple cache object that saves if not generated, e.g., using a unique
+        // identifier based on the text/settings.
         System.out.println("Saving Wav File");
         try (FileOutputStream fos = new FileOutputStream(filename)) {
-            wavBuffer.writeTo(fos);
+            wavData.writeTo(fos);
         }
         System.out.println("File saved successfully");
 
@@ -74,10 +60,8 @@ public class pipersample {
         System.out.println("Finished playing");
 
         System.out.println("Playing Wav data from memory buffer");
-        player.playAudio(new ByteArrayInputStream(wavBuffer.toByteArray()));
+        player.playAudio(new ByteArrayInputStream(wavData.toByteArray()));
         System.out.println("Finished playing from memory");
-
-
 
 //        // you can play with the Sota built in CPlayWave system, but it clips as per prev. comment
 //        System.out.println("Playing Wav File");
@@ -88,6 +72,101 @@ public class pipersample {
 //        CPlayWave.PlayWave_wait(wavBuffer.toByteArray());
 //        System.out.println("Finished playing from memory");
 
+    }
+
+    public static class PiperRequest {
+        public final String urlString;
+        public String textToGen;
+        public double lengthScale;  // longer is slower
+        public double noiseScale;  // natural variation
+        public double noiseWScale; // phenome variation.
+        public String voice; // use from listof available voices. Set to NULL to be default.
+
+        public PiperRequest (String urlString, String textToGen, String voice, double lengthScale, double noiseScale, double noiseWScale) {
+            this.urlString = urlString;
+            this.textToGen = textToGen;
+            this.lengthScale = lengthScale;
+            this.noiseScale = noiseScale;
+            this.noiseWScale = noiseWScale;
+            this.voice = voice;
+        }
+    }
+
+    public static ByteArrayOutputStream getPiperWavData(PiperRequest piperRequest) { return getPiperWavData(piperRequest, false);}
+    public static ByteArrayOutputStream getPiperWavData(PiperRequest piperRequest, boolean debug) {
+        ByteArrayOutputStream result = null;
+        try {
+            if (debug) System.out.println("Constructing the json RPC request");
+            URL url = new URL(piperRequest.urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+
+            // JSON
+            String voiceLine = piperRequest.voice == null ? "": "\"voice\": \""+piperRequest.voice+"\"";
+            String jsonInputString = "{\"text\": \"" + piperRequest.textToGen + "\"," +   // see https://github.com/OHF-Voice/piper1-gpl/blob/main/docs/API_HTTP.md
+                    voiceLine+", "+
+                    "\"sample_rate\": 22050," +  // ensure match to Sota's working rate
+                    "\"length_scale\": "+ piperRequest.lengthScale +"," +
+                    "\"noise_scale\": "+ piperRequest.noiseScale +"," +
+                    "\"noise_w_scale\": "+piperRequest.noiseWScale+"}";
+            if (debug) System.out.println("Connecting...");
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                if (debug) System.out.println("Sending Request...");
+                os.write(input, 0, input.length);
+            }
+
+            if (debug) System.out.println("Reading response...");
+            ByteArrayOutputStream wavBuffer = new ByteArrayOutputStream();
+            try (InputStream in = conn.getInputStream()) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = in.read(buffer)) != -1) {
+                    wavBuffer.write(buffer, 0, bytesRead);
+                }
+            }
+            if (debug) System.out.println("Response received successfully");
+            result = wavBuffer;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void listPiperVoices(String urlString) {
+        ByteArrayOutputStream result = null;
+        try {
+            System.out.println("Constructing the json RPC request");
+            URL url = new URL(urlString+"/voices");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            System.out.println("Connecting...");
+            int status = conn.getResponseCode();
+            InputStream in = (status >= 200 && status < 300)
+                    ? conn.getInputStream()
+                    : conn.getErrorStream();
+
+            System.out.println("HTTP status: " + status);
+
+            try (BufferedReader reader =
+                         new BufferedReader(new InputStreamReader(in, "UTF-8"))) {
+
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    response.append(line).append('\n');
+                }
+
+                System.out.println(response.toString());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -104,62 +183,34 @@ public class pipersample {
 
         public void playAudio(ByteArrayInputStream wavbuffer) {  // file as .wav
             try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(wavbuffer)) {
+                playAudio(audioStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void playAudio(AudioInputStream audioStream) {
+            try {
                 AudioFormat format = audioStream.getFormat();
-                int frameSize = format.getFrameSize();
                 SourceDataLine line = AudioSystem.getSourceDataLine(format, mixer);
-                System.out.println(format.isBigEndian());
+
                 line.open(format);
                 line.start();
-
-                ByteArrayOutputStream pcmOut = new ByteArrayOutputStream();
-                byte[] buf = new byte[1000*frameSize];
-                int n;
-                while ((n = audioStream.read(buf)) != -1) {
-                    pcmOut.write(buf, 0, n);
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                int bytesWritten = 0;
+                while ((bytesRead = audioStream.read(buffer)) != -1) {
+                    line.write(buffer, 0, bytesRead);
+                    bytesWritten+=bytesRead;
                 }
-//
-//                byte[] buffer = new byte[100*frameSize];
-//                int bytesRead;
-//                while ((bytesRead = audioStream.read(buffer)) != -1) {
-//                    // audioStream.read always reads an integer number of frames. so any leftover bytes
-//                    // mroe than a frame would be end of the file.
-//                    int fullFrameBytes = bytesRead - bytesRead%frameSize;  // trim leftover extra bytes
-//                    line.write(buffer, 0, fullFrameBytes);
-//                }
 
-                byte[] pcmBytes = (pcmOut.toByteArray());
-                System.out.println(frameSize);
-                final int FADE_FRAMES = (int)(format.getSampleRate() * 0.05); // 50ms
-                System.out.println(FADE_FRAMES);
+                int extraNeeded = line.getBufferSize() - bytesWritten%line.getBufferSize();
+                byte[] extraZeros = new byte[extraNeeded];
+                Arrays.fill(extraZeros, (byte)0);
+                line.write(extraZeros, 0, extraZeros.length);
 
-                int idxStart = pcmBytes.length - FADE_FRAMES*frameSize;
-                int maxidx = idxStart;
-                for (int i = 0; i < FADE_FRAMES; i++) {
-                    float gain = 1.0f - (float) i / FADE_FRAMES;
-
-                    int idx = idxStart + frameSize*i;
-                    short sample = (short) (
-                            (pcmBytes[idx + 1] << 8) | (pcmBytes[idx] & 0xff)
-                    );
-
-//                    System.out.println(idx+"  "+gain+"  sample before: "+sample);
-
-                    sample = (short) (sample * gain);
-//                    System.out.println("then "+sample);
-
-                    pcmBytes[idx]     = (byte) (sample & 0xff);
-                    pcmBytes[idx + 1] = (byte) ((sample >> 8) & 0xff);
-                    maxidx = Math.max(maxidx, idx);
-                }
-                System.out.println("max idx] "+maxidx+ " "+pcmBytes.length);
-                line.write(pcmBytes, 0, pcmBytes.length-(int)(format.getSampleRate() * 0.01)); // 10ms);
-//                Arrays.fill(buffer, (byte) 0);
-//                line.write(buffer, 0, buffer.length);
-
-//                line.drain();
-//                line.flush();
-//                line.close();
-                while(true);
+                line.drain();
+                line.close();
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -173,21 +224,9 @@ public class pipersample {
             }
 
             File audioFile = new File(filename);
-            try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile)) {
-                AudioFormat format = audioStream.getFormat();
-                SourceDataLine line = AudioSystem.getSourceDataLine(format, mixer);
-
-                line.open(format);
-                line.start();
-
-                byte[] buffer = new byte[4096];
-                int bytesRead;
-                while ((bytesRead = audioStream.read(buffer)) != -1) {
-                    line.write(buffer, 0, bytesRead);
-                }
-
-                line.drain();
-                line.close();
+            try {
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+                playAudio(audioStream);
             } catch (Exception e) {
                 e.printStackTrace();
             }
